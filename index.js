3de07d28b8a2e962da3b530be9225cf9e699efa2
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-12 01:03:32 +0800

    debug

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(cors({
 }));
 
 // Enable pre-flight for all routes - additional protection
-app.options('*', (req, res) => {
+app.options(allowedOrigins, (req, res) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
