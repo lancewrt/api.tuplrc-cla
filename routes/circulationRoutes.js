1746b7d9e5ff40d29785c7a442ab17c79e10f951
Lance <lancewrt@gmail.com>
2025-04-11 21:01:49 +0800

    s

--- routes/circulationRoutes.js
+++ routes/circulationRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { checkIn, checkinSearch, checkOut, checkoutRecord, checkoutSearch } from "../controller/circulationController.js";
 
-    const router = express.Router();
+const router = express.Router();
 
-    router.get('/checkout/search', checkoutSearch);
-    router.get('/checkin/search', checkinSearch);
-    router.get('/checkout-record', checkoutRecord)
-    router.post('/checkin',  (req, res) => checkIn(req, res, wss));
-    router.post('/checkout', (req, res) => checkOut(req, res, wss));  
+router.get('/checkout/search', checkoutSearch);
+router.get('/checkin/search', checkinSearch);
+router.get('/checkout-record', checkoutRecord)
+router.post('/checkin',  (req, res) => checkIn(req, res, wss));
+router.post('/checkout', (req, res) => checkOut(req, res, wss));  
 
-    return router; 
+export default router;
 
 
