4337a4fa1fd933f24a7426578bb3c0b44d7d7452
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-29 10:00:01 +0800

    update api

--- controller/attendanceController.js
+++ controller/attendanceController.js
@@ -2,9 +2,8 @@ import { db } from "../config/db.js";
 
 export const attendance = (req, res) => {
   const studentId = req.body.studentId;
-  // const date = req.body.date;
-  // const time = req.body.time;
-  console.log('student id from port: ', studentId)
+  const date = req.body.date;
+  const time = req.body.time;
  
   if (!studentId) {
     return res.status(400).json({ success: false, message: "Student ID is required." });
@@ -25,16 +24,14 @@ export const attendance = (req, res) => {
     const studentName = `${results[0].patron_fname} ${results[0].patron_lname}`;
 
     const logAttendanceQuery = "INSERT INTO attendance (patron_id) VALUES (?)";
-    db.query(logAttendanceQuery, [patronId], (err) => {
+    db.query(logAttendanceQuery, [time, date, patronId], (err) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ success: false, message: "Failed to log attendance." });
       }
-
-      console.log('logged')
       
       // Use the io instance from the request object
-      req.io.emit('attendanceUpdated', {
+      req.io.emit('attendanceUpdated',{
         success: true,
         studentName: studentName,
         message: "Attendance logged successfully.",
